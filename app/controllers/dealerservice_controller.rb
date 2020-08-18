ec6c776bc69f3269d5b2794842cfc7acde3b96ff
Pwako11 <akolo@hotmail.com>
2020-08-18 10:57:05 -0400

    Added index and show views to Dealerservice

---
+++ app/controllers/dealerservice_controller.rb
@@ -0,0 +1,13 @@
+class DealerServiceController < ApplicationController 
+
+      
+    get '/dealerservices' do
+      @services = DealerService.all
+      erb :'dealerservices/index'
+    end
+  
+    get '/dealerservices/:id' do
+        @service = DealerService.find_by_id(params[:id])
+        erb :'dealerservices/show'
+    end 
+  end
